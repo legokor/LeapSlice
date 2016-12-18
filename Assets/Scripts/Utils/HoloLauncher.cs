36af2a928d63abbe54b97b935ebabc90c1bf25a7
Bence Sgánetz <VoidXH@users.noreply.github.com>
2016-12-18 12:58:29 +0100

    Air tap implementation

--- Assets/Scripts/Utils/HoloLauncher.cs
+++ Assets/Scripts/Utils/HoloLauncher.cs
@@ -1,9 +1,13 @@
 ﻿using UnityEngine;
+using UnityEngine.VR.WSA.Input;
 
 /// <summary>
 /// HoloLens módban indítja a játékot.
+/// Ami még kell, hogy működjön: a HoloToolkit-Unity-ből egy kurzor és egy spatial mapping.
 /// </summary>
 public class HoloLauncher : Singleton<HoloLauncher> {
+    [Tooltip("A játéktér elhelyezését segítő kurzor.")]
+    public GameObject Cursor;
     [Tooltip("Játékmező, amit a játékos elhelyez, amikor felületre kattint.")]
     public GameObject GameField;
 
@@ -11,6 +15,13 @@ public class HoloLauncher : Singleton<HoloLauncher> {
     /// Ha nem falra kattintott a játékos, ennyi ideig tudassa vele a képernyőn.
     /// </summary>
     float FailTimer = 0;
+
+    /// <summary>
+    /// Rendelje hozzá a koppintáshoz a játéktér elhelyezését.
+    /// </summary>
+    void Start() {
+        InteractionManager.SourcePressed += AirTap;
+    }
 
     /// <summary>
     /// Instrukciók/hibaüzenet kijelzése.
@@ -22,15 +33,18 @@ public class HoloLauncher : Singleton<HoloLauncher> {
     }
 
     /// <summary>
-    /// Falra kattintás kezelése. Meghívandó HoloToolkit-ből.
+    /// Falra kattintás kezelése.
     /// </summary>
-    public void OnClick() {
+    public void AirTap(InteractionSourceState state) {
         RaycastHit Hit;
         if (Physics.Raycast(Camera.main.ViewportPointToRay(new Vector3(.5f, .5f)), out Hit)) {
             GameField.transform.position = Hit.point + Hit.normal * .1f;
             GameField.transform.LookAt(Hit.point);
             GameField.SetActive(true);
+            InteractionManager.SourcePressed -= AirTap;
             Destroy(this);
+            if (Cursor)
+                Destroy(Cursor);
         } else
             FailTimer = 5;
     }
