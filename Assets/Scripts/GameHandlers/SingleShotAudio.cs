80ac4432574c798044739828e8c1e382d9c635ef
VoidX <voidxb@gmail.com>
2017-11-08 21:27:12 +0100

    Audio engine choice by a #define

--- Assets/Scripts/GameHandlers/SingleShotAudio.cs
+++ Assets/Scripts/GameHandlers/SingleShotAudio.cs
@@ -1,4 +1,9 @@
-﻿using Cavern;
+﻿#define CAVERN
+
+#if CAVERN
+using Cavern;
+#endif
+
 using UnityEngine;
 
 /// <summary>
@@ -24,55 +29,18 @@ public static class SingleShotAudio {
     }
 
     /// <summary>
-    /// Hang lejátszása egy objektumon, Cavern motorral.
-    /// </summary>
-    /// <param name="Clip">Hang</param>
-    /// <param name="Obj">Objektum</param>
-    static void AttachWithCavern(AudioClip Clip, GameObject Obj) {
-        AudioSource3D Source = Obj.AddComponent<AudioSource3D>();
-        Source.Clip = Clip;
-        Source.VolumeRolloff = Rolloffs.Disabled;
-    }
-
-    /// <summary>
-    /// Hang lejátszása egy objektumon, Unity motorral.
-    /// </summary>
-    /// <param name="Clip">Hang</param>
-    /// <param name="Obj">Objektum</param>
-    static void AttachWithUnity(AudioClip Clip, GameObject Obj) {
-        Obj.AddComponent<AudioSource>().PlayOneShot(Clip);
-    }
-
-    /// <summary>
     /// Hang lejátszása egy objektumon.
     /// </summary>
     /// <param name="Clip">Hang</param>
     /// <param name="Obj">Objektum</param>
     public static void Attach(AudioClip Clip, GameObject Obj) {
-        if (UseCavern)
-            AttachWithCavern(Clip, Obj);
-        else
-            AttachWithUnity(Clip, Obj);
-    }
-
-    /// <summary>
-    /// Hang lejátszása Cavern használatával.
-    /// </summary>
-    /// <param name="Clip">Hang</param>
-    /// <param name="At">Pozíicó</param>
-    static void PlayWithCavern(AudioClip Clip, Vector3 At) {
-        AudioSource3D Source = CreateContainer(Clip, At).AddComponent<AudioSource3D>();
+#if CAVERN
+        AudioSource3D Source = Obj.AddComponent<AudioSource3D>();
         Source.Clip = Clip;
         Source.VolumeRolloff = Rolloffs.Disabled;
-    }
-
-    /// <summary>
-    /// Hang lejátszása a Unity hangmotorjával.
-    /// </summary>
-    /// <param name="Clip">Hang</param>
-    /// <param name="At">Pozíció</param>
-    static void PlayWithUnity(AudioClip Clip, Vector3 At) {
-        CreateContainer(Clip, At).AddComponent<AudioSource>().PlayOneShot(Clip);
+#else
+        Obj.AddComponent<AudioSource>().PlayOneShot(Clip);
+#endif
     }
 
     /// <summary>
@@ -81,9 +49,12 @@ public static class SingleShotAudio {
     /// <param name="Clip">Hang</param>
     /// <param name="At">Pozíció</param>
     public static void Play(AudioClip Clip, Vector3 At) {
-        if (UseCavern)
-            PlayWithCavern(Clip, At);
-        else
-            PlayWithUnity(Clip, At);
+#if CAVERN
+        AudioSource3D Source = CreateContainer(Clip, At).AddComponent<AudioSource3D>();
+        Source.Clip = Clip;
+        Source.VolumeRolloff = Rolloffs.Disabled;
+#else
+        CreateContainer(Clip, At).AddComponent<AudioSource>().PlayOneShot(Clip);
+#endif
     }
 }
